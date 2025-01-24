869afaf6edae2347b217b04b362ff09db455d8f6
dua00006 <dua00006@algonquinlive.com>
2025-01-24 17:37:04 -0500

    Updated Pulumi configuration to deploy Docker container with new image tag v0.2.0. Fixed registryName validation issue by modifying the resource name to comply with Azure naming constraints. Deployed new resources: Docker image cst8918a03dua00006-image built and pushed to ACR. Azure Container Instance cst8918a03dua00006-container-group created for running the container.

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,1 +1,115 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from '@pulumi/pulumi';
+import * as resources from '@pulumi/azure-native/resources';
+import * as containerregistry from '@pulumi/azure-native/containerregistry';
+import * as docker from '@pulumi/docker';
+import * as containerinstance from '@pulumi/azure-native/containerinstance';
+
+// Load configuration variables
+const config = new pulumi.Config();
+const appPath = config.require('appPath');
+const prefixName = config.require('prefixName');
+const imageName = prefixName;
+const imageTag = config.require('imageTag');
+const containerPort = config.requireNumber('containerPort');
+const publicPort = config.requireNumber('publicPort');
+const cpu = config.requireNumber('cpu');
+const memory = config.requireNumber('memory');
+
+// Create a resource group
+const resourceGroup = new resources.ResourceGroup(`${prefixName}-rg`);
+
+// Create the container registry
+const registry = new containerregistry.Registry(`${prefixName}ACR`, {
+  resourceGroupName: resourceGroup.name,
+  adminUserEnabled: true,
+  sku: {
+    name: containerregistry.SkuName.Basic,
+  },
+});
+
+// Get the authentication credentials for the container registry
+const registryCredentials = containerregistry
+  .listRegistryCredentialsOutput({
+    resourceGroupName: resourceGroup.name,
+    registryName: registry.name,
+  })
+  .apply((creds) => ({
+    username: creds.username!,
+    password: creds.passwords![0].value!,
+  }));
+
+// Define the container image for the service
+const image = new docker.Image(`${prefixName}-image`, {
+  imageName: pulumi.interpolate`${registry.loginServer}/${imageName}:${imageTag}`,
+  build: {
+    context: appPath,
+    platform: 'linux/amd64',
+  },
+  registry: {
+    server: registry.loginServer,
+    username: registryCredentials.username,
+    password: registryCredentials.password,
+  },
+});
+
+// Create a container group in Azure Container App service
+const containerGroup = new containerinstance.ContainerGroup(
+  `${prefixName}-container-group`,
+  {
+    resourceGroupName: resourceGroup.name,
+    osType: 'linux',
+    restartPolicy: 'always',
+    imageRegistryCredentials: [
+      {
+        server: registry.loginServer,
+        username: registryCredentials.username,
+        password: registryCredentials.password,
+      },
+    ],
+    containers: [
+      {
+        name: imageName,
+        image: image.imageName,
+        ports: [
+          {
+            port: containerPort,
+            protocol: 'tcp',
+          },
+        ],
+        environmentVariables: [
+          {
+            name: 'PORT',
+            value: containerPort.toString(),
+          },
+          {
+            name: 'WEATHER_API_KEY',
+            value: '4e631c7ef8996698f921dd9448c9fb0e', // Replace this with your real key
+          },
+        ],
+        resources: {
+          requests: {
+            cpu: cpu,
+            memoryInGB: memory,
+          },
+        },
+      },
+    ],
+    ipAddress: {
+      type: containerinstance.ContainerGroupIpAddressType.Public,
+      dnsNameLabel: `${imageName}`,
+      ports: [
+        {
+          port: publicPort,
+          protocol: 'tcp',
+        },
+      ],
+    },
+  },
+);
+
+// Export the service's IP address, hostname, and fully-qualified URL.
+export const hostname = containerGroup.ipAddress.apply((addr) => addr!.fqdn!)
+export const ip = containerGroup.ipAddress.apply((addr) => addr!.ip!)
+export const url = containerGroup.ipAddress.apply(
+  (addr) => `http://${addr!.fqdn!}:${containerPort}`,
+)
